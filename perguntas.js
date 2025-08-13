99e93f11f8c83a59eaf4e23587e5323fab7faf01
lucas170109 <lucas.kauan.oliveira@escola.pr.gov.br>
2025-08-13 10:45:41 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'GREMIO',
+    'QUANDO FOI FUNDADO?',
+    '15 DE SETEMBRO DE 1903'
+)
+criaCartao(
+    'GREMIO',
+    'QUANTAS LIBERTADORES?',
+    'TEM TRES LIBERTADORES SENDO UM DOS MAIORES CAMPEOES DA LIBERTADORES DO BRASIL GRÊMIO - 1983, 1995 e 2017 (vice em 1984 e 2007)'
+)
+criaCartao(
+    'GREMIO',
+    'QUAIS SÃO OS MAIORES IDOLOS DO GREMIO?',
+    'O Grêmio possui vários ídolos na sua história, com destaque para Renato Gaúcho, considerado o maior ídolo do clube, seguido por outros nomes importantes como Tarciso "Flecha Negra", Jardel, Danrlei e Hugo de León. '
+)
+criaCartao(
+    'GREMIO',
+    'QUANTOS TITULOS O GREMIO TEM EM SUA HISTORIA?',
+    'O Grêmio possui um total de 68 títulos em sua história '
+)
+criaCartao(
+    'GREMIO',
+    'QUNTAS COPAS DO BRASIL O GREMIO TEM?',
+    'O TRICOLOR GAUCHO TEM 5 COPAS DO BRASIL '
+)
